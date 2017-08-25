486824c9901326bc3d0709743b8a8288828bb14d
GibboK <gibbok.coding@gmail.com>
2017-08-25 10:47:52 +0200

    Add change input field for title

--- app/src/Task.js
+++ app/src/Task.js
@@ -3,13 +3,23 @@ import DeleteButton from './DeleteButton.js'
 
 class Task extends Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            title: props.title
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    handleInputChange(event) {
+        this.setState({title: event.value});
+        event.preventDefault();
     }
     render() {
         return (
             <div>
                 <div>
-                    <input type="text" defaultValue={this.props.title} />
+                    <input type="text"
+                        defaultValue={this.props.title}
+                        onChange={this.handleInputChange} />
                 </div>
                 <div>
                     <DeleteButton />
