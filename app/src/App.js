669e517e659c6def31db7136e74cdbb77c0111ef
GibboK <gibbok.coding@gmail.com>
2017-08-25 09:27:50 +0200

    Add boilerplate and initial state in app

---
+++ app/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        data: [
+          {
+            id: 0,
+            title: 'Buy milk',
+            status: 0, // 0 = todo, 1 = done
+          },
+          {
+            id: 1,
+            title: 'Buy bread',
+            status: 0,
+          },
+          {
+            id: 1,
+            title: 'Buy yogurt',
+            status: 0,
+          }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
